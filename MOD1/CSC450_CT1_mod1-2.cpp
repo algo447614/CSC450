b4a57f1251e81fe686c332ec4cebf0bde643f18a
algo447614 <147664969+algo447614@users.noreply.github.com>
2024-10-20 21:05:57 -0400

    Create CSC450_CT1_mod1-2.cpp

---
+++ MOD1/CSC450_CT1_mod1-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+// Removed conio.h as it's not standard c++ and not necessary for this program
+
+// Standard namespace declaration
+using namespace std;
+
+// Main Function
+int main()
+{
+    double myMoney = 1000.50; // this should be printed out (line 17)
+
+    // Standard Output Statements
+    cout << "Please be sure to correct all syntax errors in this program" << endl;
+
+    cout << "I have corrected all errors for this program." << endl;
+
+    cout << "The total amount of money available is = " << myMoney << endl;
+
+    // Wait For Output Screen
+    cin.get(); //added
+
+    // Main Function return Statement
+    return 0;
+}
