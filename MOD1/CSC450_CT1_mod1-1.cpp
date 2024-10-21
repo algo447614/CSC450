435f72858325173107454b638b8855855850ecf3
algo447614 <147664969+algo447614@users.noreply.github.com>
2024-10-20 20:55:20 -0400

    Create CSC450_CT1_mod1-1.cpp

---
+++ MOD1/CSC450_CT1_mod1-1.cpp
@@ -0,0 +1,21 @@
+/* Simple Program with a few Errors for Correction
+   Please be sure to correct all outlined errors. --added the end of the multi-line comment  */
+
+
+#include<iostream>
+//#include<conio.h> --not a standard header file, commented out
+//Standard namespace declaration --commented out
+using namespace std;
+//Main Function
+int main()
+{
+
+     //Standard Ouput Statement
+     cout<<"Welcome to this C++ Program"<<endl;
+
+     cout<<"I have corrected all errors for this program." <<endl; //Added quotation marks
+     // Wait For Output Screen
+     cin.get(); //Added
+     //Main Function return Statement
+     return 0; //Added
+}
